7112b254aa7334543c9486c8efbdc660a8255a95
Ricardo Ramos <ramosricardo96@gmail.com>
2019-03-14 19:35:57 -0700

    Update App.js

--- handstand-app/App.js
+++ handstand-app/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 import { withAuthenticator } from 'aws-amplify-react-native'
 
@@ -7,24 +7,35 @@ import Amplify from '@aws-amplify/core'
 import config from './aws-exports'
 Amplify.configure(config)
 
+import {createBottomTabNavigator, createStackNavigator, createSwitchNavigator, createAppContainer} from 'react-navigation'
+import {Ionicons} from '@expo/vector-icons';
+
+import Home from './screens/Home';
+import Settings from './screens/Settings';
+import Progress from './screens/Progress';
+import Camera from './screens/Camera';
+
+const CameraNav = createSwitchNavigator({
+    Camera: {screen: Camera},
+});
+
+const LoggedIn = createBottomTabNavigator(
+  {
+    Home: {screen: Home},
+    Settings: {screen: Settings},
+    Progress: {screen: Progress},
+    Camera: CameraNav,
+  },
+);
+
+const MainNav = createAppContainer(LoggedIn);
+
 class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Yo!</Text>
-      </View>
+        <MainNav/>
     );
   }
 }
 
-export default withAuthenticator(App, { includeGreetings: true })
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default withAuthenticator(App, {includeGreetings: true});
